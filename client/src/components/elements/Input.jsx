1abd7393ba295a70db92cb0917fc50b402544831
Stephen-Ibe <saintkalson@gmail.com>
2022-07-18 23:49:30 +0100

    updated input

--- client/src/components/elements/Input.jsx
+++ client/src/components/elements/Input.jsx
@@ -9,7 +9,7 @@ const Input = ({ placeholder, name, type, handleChange, value }) => {
       onChange={(e) => handleChange(e)}
       step='0.0001'
       value={value}
-      className='my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white'
+      className='my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism placeholder:text-sm'
     />
   );
 };
