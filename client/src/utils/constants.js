accca2058600db780c83dc20e5b38a15abe0e6ee
Stephen-Ibe <saintkalson@gmail.com>
2022-07-25 08:59:38 +0100

    deployed smart_contract and setup transactionABI and contractAddress in the FE

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x729B6295Ab00E3e15bc3A926cE351BD1DC068f73';
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-waffle');
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: '0.8.9',
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/6i5m6uJXaQqxlytzeJBwJM1a_KF-N31s',
+      accounts: [
+        '8f238bbfc30c2470be0ab3ec377a71405e28a32cb33bab2a8ec18abd06829f32',
+      ],
+    },
+  },
 };
