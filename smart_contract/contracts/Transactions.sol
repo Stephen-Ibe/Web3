5ebfb837cce6c038f64d9f8e71badab8de0cff66
Stephen-Ibe <saintkalson@gmail.com>
2022-07-25 06:23:58 +0100

    smart_contract complete

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.9;
 
 contract Transactions {
-    uint256 transactionCounter;
+    uint256 transactionCount;
 
     event Transfer(
         address from,
@@ -25,13 +25,43 @@ contract Transactions {
 
     TransferStruct[] transactions;
 
-    function addToBlockchain() public {}
+    function addToBlockchain(
+        address payable receiver,
+        uint256 amount,
+        string memory message,
+        string memory keyword
+    ) public {
+        transactionCount += 1;
+        transactions.push(
+            TransferStruct(
+                msg.sender,
+                receiver,
+                amount,
+                message,
+                block.timestamp,
+                keyword
+            )
+        );
+
+        emit Transfer(
+            msg.sender,
+            receiver,
+            amount,
+            message,
+            block.timestamp,
+            keyword
+        );
+    }
 
     function getAllTransactions()
         public
         view
         returns (TransferStruct[] memory)
-    {}
+    {
+        return transactions;
+    }
 
-    function getTransactionCount() public view returns (uint256) {}
+    function getTransactionCount() public view returns (uint256) {
+        return transactionCount;
+    }
 }
